421a0c69aec2eacee0a9baa623f28108a08baad7
Christian <engineering4@voltumotor.com>
2020-10-15 08:03:50 -0300

    Creo modelo de componente electronico

---
+++ src/app/model/elec_comp.ts
@@ -0,0 +1,15 @@
+export class Producto{
+  public internal_code:string;
+  public manf_number: string;
+  public manufacturer: string;
+  public description: string;
+  public digikey_link: string;
+  public image: string;
+  public price: number;
+  public stock: number;
+  public datasheet: string;
+  public category: string;
+  public package: string;
+  public value: number;
+  public unit: string;
+}
