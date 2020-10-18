c8cc7eae61935d7be0d6b0159b91e4dd89b271a2
Christian <engineering4@voltumotor.com>
2020-10-18 13:10:20 -0300

    Creo página principal con lista de componentes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ComponentServiceService } from '../component-service.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private components;
 
-  constructor() {}
+  constructor(private CompServ: ComponentServiceService) {
+    CompServ.getAllComponents().subscribe(datos => { this.components = datos });
+  }
 
 }
