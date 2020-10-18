a98fd56ec070f0c15da9f0a16167779869593e85
Christian <engineering4@voltumotor.com>
2020-10-18 17:14:58 -0300

    Corrijo enrutado e inicio pagina de componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,22 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    children: [{
+      path: "",
+      loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+      
+    },
+    {
+      path: ":internal_code",
+      loadChildren: () => import('./elec-comp/elec-comp.module').then( m => m.ElecCompPageModule)
+      
+    },
+    {
+      path: ":pcb_code",
+      loadChildren: () => import('./pcb/pcb.module').then( m => m.PcbPageModule)
+    }
+    ]
+    
   },
   {
     path: '',
--- src/app/elec-comp/elec-comp.page.ts
+++ src/app/elec-comp/elec-comp.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ComponentServiceService } from '../component-service.service';
+import { Elec_comp } from '../model/elec_comp';
 
 @Component({
   selector: 'app-elec-comp',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ElecCompPage implements OnInit {
 
-  constructor() { }
+  private elec_comp = new Elec_comp();
+  private id_ingresado: string;
+
+  constructor(private activateRoute: ActivatedRoute, private compServ: ComponentServiceService) { }
 
   ngOnInit() {
+    this.activateRoute.paramMap.subscribe(
+      paramMap => {
+        this.compServ.getComponent(paramMap.get("internal_code"))
+          .subscribe(datos => {
+            this.elec_comp = datos;
+          });
+      });
   }
 
 }
